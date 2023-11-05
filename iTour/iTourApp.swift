737632d9b8b1ca4cedb55a0c16ffaa97246cb6c1
Storiwr <storiwr@users.noreply.github.com>
2023-11-05 11:34:05 -0500

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Stephen on 11/5/23.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
